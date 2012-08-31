d7ac9c136db99c34a9329759f5eb0a753dce2a7a
Stephen Haptonstahl <srh@haptonstahl.org>
2012-08-31 17:19:43 -0400

    Added RStudio project file

--- deCruft/deCruft.R
+++ deCruft/deCruft.R
@@ -1,9 +1,15 @@
 # deCruft
 #
 # Author: Stephen R. Haptonstahl (srh@haptonstahl.org)
-# Load using:  source("http://sheer.ucdavis.edu/svn/software/public/deCruft/deCruft.R")
+# After installing and loading 'devtools', load using
+# source_url("https://raw.github.com/shaptonstahl/R/master/deCruft/deCruft.R")
 
 # The following sapply detaches all attached objects except for packages and defaults
 result <- sapply( search()[-c( grep("Autoloads", search()), grep("GlobalEnv", search()), grep("package", search()) )],
   function(strIn) { detach( pos=grep(strIn, search())[1] ) })
 rm(list=ls())  # remove all objects
+
+
+non.default.loaded.namespaces <- rev(setdiff(loadedNamespaces(), c("base", "tools", options()$defaultPackages)))
+# sapply(non.default.loaded.namespaces, unloadNamespace)  # removes environment and from search path
+rm(list=ls(all.names=TRUE, envir=.GlobalEnv), envir=.GlobalEnv)  # clears *all* objects including visible and hidden environments
