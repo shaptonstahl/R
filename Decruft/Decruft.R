b78c8eb3f8b5998483e84273cf2a9e3cafa287ce
Stephen Haptonstahl <Stephen Haptonstahl>
2012-09-06 02:56:43 -0400

    Renamed folder for Decruft

---
+++ Decruft/Decruft.R
@@ -0,0 +1,37 @@
+# deCruft
+#
+# Author: Stephen R. Haptonstahl (srh@haptonstahl.org)
+#
+# After installing and loading 'devtools', load using
+# source_url("https://raw.github.com/shaptonstahl/R/Decruft/Decruft.R")
+#
+# or download to keep it local and handy.
+
+cat("Decruft by Stephen Haptonstahl (srh@haptonstahl.org)\n\n")
+rm(list=ls(all.names=TRUE, envir=.GlobalEnv), envir=.GlobalEnv)  # clears *all* objects including visible and hidden environments
+
+max.fails <- 500
+
+GetNondefaultLoadedPackages <- function() {
+  setdiff(loadedNamespaces(), c("base", "tools", options()$defaultPackages))
+}
+
+fail.counter <- 0
+while( length(pkgs.to.remove <- GetNondefaultLoadedPackages()) > 0 ) {
+  res <- tryCatch( unloadNamespace(sample(pkgs.to.remove, 1)),
+                   error=function(e) e,
+                   warning=function(w) w)
+  if( !is.null(res) ) fail.counter <- fail.counter + 1
+  if(fail.counter >= max.fails) break
+}
+
+if(fail.counter >= max.fails) {
+  cat("Unable to remove all package environments from the search() path.",
+      "You may want to restart R to guarantee a clean session.\n\n", 
+      sep="\n")
+} else {
+  cat("All packages unloaded.\n\n")
+}
+
+rm(list=ls(all.names=TRUE, envir=.GlobalEnv), envir=.GlobalEnv)  # clears *all* objects including visible and hidden environments
+cat("All objects deleted, including hidden package environments.")
