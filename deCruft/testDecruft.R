b0f4ceb73d70c907292f05160292f7ce69f9002d
Stephen Haptonstahl <Stephen Haptonstahl>
2012-09-06 02:38:23 -0400

    Version 1.0.  This puppy is robust.

---
+++ deCruft/testDecruft.R
@@ -0,0 +1,13 @@
+# Test Decruft
+
+# setwd("C:/Users/Steve/Documents/GitHub/R/Decruft")
+
+library(MASS)
+library(snowfall)
+library(MCMCpack)
+library(AER)
+
+source("Decruft.R")
+
+ls(all.names=TRUE)
+search()
