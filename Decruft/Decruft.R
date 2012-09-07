213420c00d894317fa49d419aa75d03fb89ecbcd
Stephen Haptonstahl <srh@haptonstahl.org>
2012-09-07 17:21:07 -0400

    Tweaks to Decruft

--- Decruft/Decruft.R
+++ Decruft/Decruft.R
@@ -3,11 +3,17 @@
 # Author: Stephen R. Haptonstahl (srh@haptonstahl.org)
 #
 # After installing and loading 'devtools', load using
-# source_url("https://raw.github.com/shaptonstahl/R/Decruft/Decruft.R")
-#
-# or download to keep it local and handy.
+#   source_url("https://raw.github.com/shaptonstahl/R/master/Decruft/Decruft.R")
+# or install using 'source' if you download it and keep it handy.
+# 
+# I add the following lines to the top of each script (below name, author):
+#####  Awesome decrufting  #####
+if( !("devtools" %in% installed.packages()[,"Package"] ) ) install.packages("devtools")
+library(devtools)
+source_url("https://raw.github.com/shaptonstahl/R/master/Decruft/Decruft.R")
+# end decrufting
 
-cat("Decruft by Stephen Haptonstahl (srh@haptonstahl.org)\n\n")
+cat("\nDecruft by Stephen Haptonstahl (srh@haptonstahl.org)\n\n")
 rm(list=ls(all.names=TRUE, envir=.GlobalEnv), envir=.GlobalEnv)  # clears *all* objects including visible and hidden environments
 
 max.fails <- 500
---
+++ FirstLetterCapitalize/FirstLetterCapitalize.R
@@ -0,0 +1,33 @@
+# FirstLetterCapitalize
+
+FirstLetterCapitalize <- function(x, 
+                                  lower.except.inner=c("a", "an", "the", 
+                                                       "and", "or", "but", "as", "both", "for", "how", "if", "nor", "now", "so", "than", "that", "till", "when", "yet",
+                                                       "to", "at", "in", "with", "by", "down", "from", "into", "lest", "like", "mid", "near", "next", "of", "off", "on", "onto", "out", "over", "pace", "past", "per", "plus", "pro", "qua", "sans", "save", "than", "unto", "up", "upon", "via", "vice", "with", "ere"),
+                                  lower.always,
+                                  upper.always) {
+  stopifnot(is.character(x <- as.character(x)),
+            is.character(lower.except.inner),
+            missing(lower.always) || is.character(lower.always),
+            missing(upper.always) || is.character(upper.always) )
+  out <- gsub(pattern="([a-zA-Z])(\\w+)", 
+              replacement="\\U\\1\\E\\L\\2\\E", 
+              x, perl=TRUE)
+  out <- gsub(pattern=paste("([^^]+)(", paste(lower.except.inner, collapse="|"), ")([^$]+)", sep=""), 
+              replacement="\\1\\L\\2\\E\\3", 
+              ignore.case=TRUE,
+              x=out, perl=TRUE)
+  if( !missing(lower.always) ) {
+    out <- gsub(pattern=paste("\\b(", paste(lower.always, collapse="|"), ")\\b", sep=""), 
+                replacement="\\L\\1\\E",
+                ignore.case=TRUE,
+                x=out, perl=TRUE)
+  }
+  if( !missing(upper.always) ) {
+    out <- gsub(pattern=paste("\\b(", paste(upper.always, collapse="|"), ")\\b", sep=""), 
+                replacement="\\U\\2\\E", 
+                ignore.case=TRUE,
+                x=out, perl=TRUE)
+  }
+  return( out )
+}
