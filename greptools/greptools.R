7ee70d5bc4157b7e7f96a83a982193c5a61b2413
Stephen Haptonstahl <srh@haptonstahl.org>
2015-07-27 08:48:07 -0400

    Added greptools

---
+++ greptools/greptools.R
@@ -0,0 +1,23 @@
+# c
+#
+# Author: Stephen R. Haptonstahl (srh@haptonstahl.org)
+# Source: https://github.com/shaptonstahl/R
+#
+# Call with:
+#   source("http://www.haptonstahl.org/R/greptools/greptools.R")
+
+cat("\ngreptools by Stephen Haptonstahl (srh@haptonstahl.org)\n\n")
+
+grepstring <- function(pattern, x, ...) {
+  #' Given a regular expression and a character vector
+  #' return the first match in each element or "" if there 
+  #' is no match
+  return(sapply(x, function(this.x) {
+    re <- regexpr(pattern, this.x, ...)
+    if(re > 0) {
+      return(substring(this.x, re, re + attr(re, "match.length") - 1))
+    } else {
+      return("")
+    }
+  }))
+}
